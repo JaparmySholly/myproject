21ab6bccdfb8b19a46478ed64f3ec36f5502e1f7
JaparmySholly <japarmysholly@gmail.com>
2022-11-14 20:17:05 +0100

    first commit

---
+++ 02Basics/02Variables.js
@@ -0,0 +1,20 @@
+const uid = "abc123";
+
+var fullName = "japarmy Sholly";
+var email ="japarmysholly@gmail.com"
+var password = '123456'
+var confirmPassword = '123456'
+var courseCount = 0
+var isLoggedInFromGoogle = false;
+
+console.log(fullName);
+console.log(email);
+console.log(password);
+console.log(uid);
+
+console.log(`
+Name is: ${fullName}
+ID: ${uid}
+Email: ${email}
+Password: ${password}
+`)
---
+++ 02Basics/03Operators.js
@@ -0,0 +1,4 @@
+var num1 = 7;
+var num2 = 6;
+
+console.log(num1 + num2);
